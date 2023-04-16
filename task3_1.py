b003d34215a834ca879a8ba0fa6f29d79af54fe4
JanVanAik <ar.orlovskiy@gmail.com>
2023-04-16 17:50:47 +0300

    first task done

--- task3_1.py
+++ task3_1.py
@@ -4,16 +4,23 @@ import random
 
 userNum = int(input('Введите число N: '))
 targetNumber = int(input('Введите искомое число: '))
-userList = [random.randint(1, userNum) for i in range(userNum + 1)]
+randomList = [random.randint(1, userNum) for i in range(userNum + 1)]
 count = 0
-for i in userList:
+for i in randomList:
     if i == targetNumber:
         count += 1
 
-print(f'В заданном массиве вида "1..N"  {userList} искомое число {targetNumber} встречается {count} раз ')
+print(f'В заданном массиве вида "1..N"  {randomList} искомое число {targetNumber} встречается {count} раз ')
 
 
 # Если массив вводит пользователь
 
-userNum = int(input('Введите число N: '))
-targetNumber = int(input('Введите искомое число: '))
+upperBoard = int(input('Введите число N: '))
+selectedNumber = int(input('Введите искомое число: '))
+userList = [int(input('Введите следующее число массива: ')) for i in range(upperBoard)]
+counter = 0
+for i in userList:
+    if i == selectedNumber:
+        counter += 1
+
+print(f'В заданном массиве вида "1..N"  {userList} искомое число {selectedNumber} встречается {counter} раз ')
